eb3eebc0ee8944458925606285e113d257887713
bandasaiakshaya-star <bandasaiakshaya@gmail.com>
2025-09-02 18:18:13 -0600

    TICKET3: enforcing client header and add to context

--- src/envelop/buildHeaders.ts
+++ src/envelop/buildHeaders.ts
@@ -1,12 +1,13 @@
 import type { Plugin } from '@envelop/core';
 import { v4 as uuid } from 'uuid';
+import { GraphQLError } from 'graphql';
 import { ContextType } from '../types';
 
-export const buildHeaders = (): Plugin<ContextType> => {
-  return {
-    onContextBuilding({ context, extendContext }) {
-      const requestId = uuid();
-      extendContext({ requestId: requestId });
-    },
-  };
-};
+export const buildHeaders = (): Plugin<ContextType> => ({
+  onContextBuilding({ context, extendContext }) {
+    const requestId = uuid();
+    const client = (context as any)?.request?.headers?.get('client') || '';
+    if (!client) throw new GraphQLError('Missing required header "client"');
+    extendContext({ requestId: requestId });
+  },
+});
